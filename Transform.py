b5cda344664a2ea8bd71ed0823c1da3dc74d0335
FelipeMac <felipe.ojeda19@gmail.com>
2023-07-02 19:10:46 -0400

    fixing readme.md

--- Transform.py
+++ Transform.py
@@ -1,5 +1,5 @@
 from PIL import Image
 
-img = Image.open('input.png')
+img = Image.open('/Users/felipeojeda/Desktop/Proyectos/yumisite.com/weberpaxPro/APP/myapp/public/assets/templates/01a/profile.png')
 rgb_im = img.convert('RGB')
-rgb_im.save('output.jpg', 'JPEG')
+rgb_im.save('/Users/felipeojeda/Desktop/Proyectos/yumisite.com/weberpaxPro/APP/myapp/public/assets/templates/01a/profile.jpg', 'JPEG')
